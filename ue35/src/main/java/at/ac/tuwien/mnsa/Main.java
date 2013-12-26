86e4ff4b178d4337712b35992815331a14deb6c7
steff7 <steff7@gmx.com>
2013-12-26 12:49:17 +0100

    Created simple AT command test application

--- ue35/src/main/java/at/ac/tuwien/mnsa/Main.java
+++ ue35/src/main/java/at/ac/tuwien/mnsa/Main.java
@@ -1,7 +1,27 @@
 package at.ac.tuwien.mnsa;
 
-public class Main {
-    public static void main(String[] args) {
+import at.ac.tuwien.mnsa.comm.SerialConnection;
+import at.ac.tuwien.mnsa.comm.SerialConnection2;
 
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        SerialConnection2 serialConnection = SerialConnection2.open();
+
+        InputStream inputStream = serialConnection.getInputStream();
+        OutputStream outputStream = serialConnection.getOutputStream();
+
+        PrintWriter writer = new PrintWriter(outputStream);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        writer.print("ATD43273286790");
+        writer.print("\r\n");
+        writer.flush();
+        String string = reader.readLine();
+        System.out.println(string);
+
+        reader.close();
+        writer.close();
     }
 }
